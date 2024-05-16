685669430eaf5281cff04ba2e269f54d94841156
BumgeunSong <isp1195@gmail.com>
2024-05-16 16:25:09 +0900

    메인 화면에 TextField 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <h1>Free-writing App</h1>
+      <TextField></TextField>
     </div>
   );
 }
