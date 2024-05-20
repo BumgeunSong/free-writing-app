ff9ce7690d483650ee7a5f48a863a2fc7d509b78
BumgeunSong <isp1195@gmail.com>
2024-05-20 22:01:58 +0900

    styled 컴포넌트 추가

--- src/FreeWritingTextField.tsx
+++ src/FreeWritingTextField.tsx
@@ -1,16 +1,23 @@
 import TextField from '@mui/material/TextField';
 import { useRecoilState } from 'recoil';
 import { freeWritingText } from './atoms';
+import styled from '@emotion/styled'
+
+const TextFieldContainer = styled.div`
+    display: block;
+`
 
 function FreeWritingTextField() {
     const [text, setText] = useRecoilState(freeWritingText)
     return (
-        <TextField
-            multiline
-            value={text}
-            onChange={(event) => {
-                setText(event.target.value)
-            }} />
+        <TextFieldContainer>
+            <TextField
+                multiline
+                value={text}
+                onChange={(event) => {
+                    setText(event.target.value)
+                }} />
+        </TextFieldContainer>
     )
 }
 
